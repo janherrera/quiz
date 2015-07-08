038252158709cfcbb874d97844a4e39ad691bc3c
janherrera <janh@ono.com>
2015-07-08 16:41:35 +0200

    Integración Postgress
    Añadido módulo Postgress y cambios para integrar SQlite en desarrollo

--- models/models.js
+++ models/models.js
@@ -1,11 +1,28 @@
 var path = require('path');
 
+// Obtención parámetros DDBB
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name     = (url[6]||null);
+var user        = (url[2]||null);
+var pwd         = (url[3]||null);
+var protocol    = (url[1]||null);
+var dialect     = (url[1]||null);
+var port        = (url[5]||null);
+var host        = (url[4]||null);
+var storage     = process.env.DATABASE_STORAGE;
+
 // Cargar Modelo ORM
 var Sequelize = require('sequelize');
 
-// Usar BBDD sqlite
-var sequelize = new Sequelize(null, null, null,
-                      {dialect: "sqlite", storage: "quiz.sqlite"}
+// Usar BBDD sqlite o Postgress
+var sequelize = new Sequelize(DB_name, user, pwd,
+                       {dialect: protocol,
+                        protocol: protocol,
+                        port: port,
+                        host: host,
+                        storage: storage,   // solo SQlite
+                        omitNull: true      // solo Postgress
+                        }
                     );
 
 // Importar definión Tablas
