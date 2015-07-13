2d82048c8b4db7962eda934f789318a4e868104f
janherrera <janh@ono.com>
2015-07-13 21:27:20 +0200

    Nuevas Preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,5 +1,24 @@
 var models = require('../models/models.js');
 
+// Get /quizes/new
+exports.new = function(req, res) {
+    var quiz = models.Quiz.build(
+        {pregunta: "Pregunta", respuesta: "Respuesta"}
+    );
+
+    res.render('quizes/new', {quiz: quiz});
+};
+
+// Post /quizes/create
+exports.create = function(req, res) {
+    var quiz = models.Quiz.build( req.body.quiz );
+
+    quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+        res.redirect('/quizes');
+    })
+};
+
+
 // Autoload para pillar el código en caso de :quizId
 exports.load = function(req, res, next, quizId) {
   models.Quiz.findById(quizId).then(function(quiz) {
@@ -9,6 +28,13 @@ exports.load = function(req, res, next, quizId) {
       } else {
         next(new Error('No existe quizId=' + quizId));
       }
+  }).catch(function(error) {next(error);});
+};
+
+// Get /quizes
+exports.index = function(req, res) {
+  models.Quiz.findAll().then(function(quizes) {
+      res.render('quizes/index.ejs', {quizes: quizes});
   }).catch(function(error) {next(error);});
 };
 
