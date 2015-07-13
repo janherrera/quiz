a309fd3517ee1c9cb8277e9cda2507cb0e5aa4fe
janherrera <janh@ono.com>
2015-07-13 18:57:02 +0200

    Formulario Busquedas
    Añadido del formulario para buscar preguntas en una nueva rama del
    proyecto

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -12,11 +12,17 @@ exports.load = function(req, res, next, quizId) {
   }).catch(function(error) {next(error);});
 };
 
-// Get /quizes
+// Get /quizes with search option
 exports.index = function(req, res) {
-  models.Quiz.findAll().then(function(quizes) {
-      res.render('quizes/index.ejs', {quizes: quizes});
-  }).catch(function(error) {next(error);});
+    var busca = '%';
+    if (req.query.search != undefined) {
+        busca = '%' + req.query.search + '%';
+        busca = busca.trim().replace(/\s/g,"%");
+    }
+
+    models.Quiz.findAll({where: ["pregunta like ?", busca], order: 'pregunta ASC'}).then(function(quizes) {
+        res.render('quizes/index.ejs', {quizes: quizes});
+    }).catch(function(error) {next(error);});
 };
 
 // Get /quizes/:id
