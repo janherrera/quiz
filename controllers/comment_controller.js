deb03f3efb1ae834f861de6c2ab6cee25ddbc02e
janherrera <janh@ono.com>
2015-07-26 19:32:00 +0200

    Moderacion Comentarios

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -1,5 +1,22 @@
 var models = require('../models/models.js');
 
+// Autoload de comments
+exports.load = function(req, res, next, commentId) {
+    models.Comment.find({
+        where: {
+            id: Number(commentId)
+        }
+    }).then(function(comment){
+        if (comment) {
+            req.comment = comment;
+            next();
+        } else {
+            next(new Error('No existe commentId=' + commentId));
+        }
+      }
+    ).catch(function(error){next(error)});
+};
+
 // Get /quizes/:quizId/comments/new
 exports.new = function(req, res) {
     res.render('comments/new.ejs', {quizid: req.params.quizId, errors: []});
@@ -25,4 +42,14 @@ exports.create = function(req, res) {
             }
         }
     ).catch(function(error){next(error)});
+};
+
+
+// Get /quizes/:quizId/comments/:commentId/publish
+exports.publish = function(req, res) {
+    req.comment.publicado = true;
+
+    req.comment.save( {fields: ["publicado"]})
+        .then( function(){ res.redirect('/quizes/' + req.params.quizId);})
+        .catch(function(error){next(error)});
 };
